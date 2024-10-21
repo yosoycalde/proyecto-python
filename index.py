e03dc64b27fb90853dc92dd72c62f26331b00968
yosoycalde <juancalderon2005.18@gmail.com>
2024-10-21 15:55:57 -0500

    cal

--- index.py
+++ index.py
@@ -1,2 +1,9 @@
-print ("Bienvenido")
-print ("")
+import tkinter as tk
+ventana = tk.Tk()
+ventana.title("Codigo de calculadora")
+ventana.geometry("400x500")
+etiqueta = tk.Label(ventana, text="Mi calculadora")
+etiqueta.pack()
+ventana.mainloop()
+# print ("Bienvenido")
+# print ("En este codigo ")
