f5c6e071e472cda3350483a13065be80d30b8f0e
yosoycalde <juancalderon2005.18@gmail.com>
2024-10-21 16:51:28 -0500

    cal1

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
-import tkinter as tk
-ventana = tk.Tk()
-ventana.title("Codigo de calculadora")
-ventana.geometry("400x500")
-etiqueta = tk.Label(ventana, text="Mi calculadora")
+import tkinter
+
+ventana = tkinter.Tk()
+ventana.geometry("500x300")
+
+etiqueta = tkinter.Label(ventana, text="hola mundo" ,bg="red")
 etiqueta.pack()
+ 
+
 ventana.mainloop()
-# print ("Bienvenido")
-# print ("En este codigo ")
+
